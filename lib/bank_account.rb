5adbb0ecf2f8dbffe8172441dbe23b501e0b26a5
Cara Morelli <caramorelli7@gmail.com>
2018-10-11 19:34:10 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,23 @@
 class BankAccount
+  
+  attr_accessor :name, :balanace, :status
+  
+  def initialize(name)
+    # @bank_account = bank_account
+    @name = name
+    @balance = 1000
+    @status = 'open'
+  end 
+  
+  def balance 
+  end
+  
+  def deposit(amount)
+    @balance += amount
+  end 
+  
+  def display_balance
+    "#{self.balance}"
+  end 
 
 end
